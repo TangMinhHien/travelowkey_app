621f8bd2217d4ed798e51a77adc51933c775acac
Tlon9 <21520331@gm.uit.edu.vn>
2023-11-02 11:39:29 +0700

    Revert "Update nav_bar, search_toolbar"
    This reverts commit a383767a5e2e0d788f0385793d6927f1679fdc01.

--- app/src/main/java/com/example/nt118_project/MainActivity.kt
+++ app/src/main/java/com/example/nt118_project/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.WindowManager
 import android.widget.TextView
-import androidx.appcompat.app.ActionBar.Tab
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -13,29 +12,21 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.viewpager2.widget.ViewPager2
 import com.example.nt118_project.Fragments.BillFragment
-import com.example.nt118_project.Fragments.FragmentPageAdapter
 import com.example.nt118_project.Fragments.HomeFragment
 import com.example.nt118_project.Fragments.SearchFragment
 import com.example.nt118_project.Fragments.UserFragment
 import com.example.nt118_project.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private lateinit var tabLayout: TabLayout
-    private  lateinit var  viewPager2: ViewPager2
-    private lateinit var adapter: FragmentPageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         replaceFragment(HomeFragment())
-
 
         binding.bottomNavigationView.setOnItemSelectedListener {
 
